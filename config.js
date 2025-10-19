d5ea5db7ada75e1a5a93216187d234d5228181c4
zidnfikli-arch <zidnfikli@gmail.com>
2025-10-20 06:01:06 +0700

    Web PororoAvengers

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8208992180:AAEqyxg2XSep3GZv438cibkch0lfhnW3R4M",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7860329124", // Ubah Jadi Id Mu !!!
+  port: "22", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "http://zyygasukayaping.ymzpterodactyl.biz.id" // Ubah Jadi Ip Vps Mu !!!
+};
